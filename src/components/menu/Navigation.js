ef986f7a56c9e7eb97ae9876c843a768ad4cc59f
S0f1eM <s.marchand56@gmail.com>
2019-07-19 23:59:17 +0200

    update correcting ui semantic responsive design

--- src/components/menu/Navigation.js
+++ src/components/menu/Navigation.js
@@ -5,21 +5,16 @@ const Navigation = () => {
 
 	return (
 		
-		<div className="ui fixed menu">
+		<div className="ui fixed stackable menu">
 			  <Link to="/" className="active item">Home</Link> 
-			  <Link to="/News" className="item">News</Link>	  
-			  <Link to="/Markdown" className="item">Markdown Generator</Link>	
 		  	<div className="right menu">
-		    
-		    	<Link to="/About" className="item">About</Link>
-		     <div className="item">      
+		  		 <Link to="/News" className="item">News</Link>	  
+		  		<Link to="/Markdown" className="item">Markdown Generator</Link>	
+		    	<div className="item">      
 	             <a href="https://twitter.com/S0f1eM" target="_blank" rel="noopener noreferrer"><i className="big twitter icon"></i></a>
 	             <a href="https://www.linkedin.com/in/sophie-marchand-web-developer/" target="_blank" rel="noopener noreferrer"><i className="big linkedin icon"></i></a>
             	 <a href="https://github.com/S0f1eM" target="_blank" rel="noopener noreferrer"><i className="big github alternate icon"></i></a>
-            </div>
-            <div className="item">
-                <a href="https://twitter.com/S0f1eM?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @S0f1eM</a>
-            </div>
+            	</div>
 		  </div>
 		</div>
 	)
