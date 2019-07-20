eb2a53e8d5c10b90d6d3a3779e3bff911ab7b3f2
S0f1eM <s.marchand56@gmail.com>
2019-07-20 13:36:09 +0200

    ...back to the first githubpage edit..

--- src/components/menu/Footer.js
+++ src/components/menu/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
 
 	return (
 		<div className="ui horizontal small divided link list">
-		<p className="ui centered text">2019 - This website is made with <a href="https://fr.reactjs.org/"><i className="react large icon"></i></a> - The repository is on <a href="https://github.com/S0f1eM/React-playground"><i className="github square large icon"></i></a> - On github page click on Home to see the home page correctly</p>
+		<p className="ui centered text">2019 - This website is made with <a href="https://fr.reactjs.org/">
+		<i className="react large icon"></i></a> - The repository is on <a href="https://github.com/S0f1eM/React-playground"><i className="github square large icon"></i></a> - ****from githubpage > CLICK ON HOME****</p>
 		</div>
 	)
 }
