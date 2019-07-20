97928910a65e9249d0e93b0620b69620cbe8bc07
S0f1eM <s.marchand56@gmail.com>
2019-07-20 13:38:20 +0200

    ...back to the first githubpage edit..

--- src/components/menu/Footer.js
+++ src/components/menu/Footer.js
@@ -7,7 +7,8 @@ const Footer = () => {
 	return (
 		<div className="ui horizontal small divided link list">
 		<p className="ui centered text">2019 - This website is made with <a href="https://fr.reactjs.org/">
-		<i className="react large icon"></i></a> - The repository is on <a href="https://github.com/S0f1eM/React-playground"><i className="github square large icon"></i></a> - ****from githubpage > CLICK ON HOME****</p>
+		<i className="react large icon"></i></a> - The repository is on <a href="https://github.com/S0f1eM/React-playground"><i className="github square large icon"></i></a> 
+		- ****see nothing (github page issue I try to fix) ? > CLICK ON HOME ****</p>
 		</div>
 	)
 }
