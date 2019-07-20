b1a85294177d956c8e54a6cb7194f2e7d210438d
S0f1eM <s.marchand56@gmail.com>
2019-07-20 09:40:06 +0200

    clean navigation for production

--- src/components/menu/Navigation.js
+++ src/components/menu/Navigation.js
@@ -8,7 +8,6 @@ const Navigation = () => {
 		<div className="ui fixed stackable menu">
 			  <Link to="/" className="active item">Home</Link> 
 		  	<div className="right menu">
-		  		 <Link to="/News" className="item">News</Link>	  
 		  		<Link to="/Markdown" className="item">Markdown Generator</Link>	
 		    	<div className="item">      
 	             <a href="https://twitter.com/S0f1eM" target="_blank" rel="noopener noreferrer"><i className="big twitter icon"></i></a>
