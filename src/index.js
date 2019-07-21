b61da26eaeeaab74ed7029ec979ebca2452c2039
S0f1eM <s.marchand56@gmail.com>
2019-07-21 16:15:28 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import App from './components/App';
-import reducers from './reducers';
-import promise from 'redux-promise';
-import './css/App.css';
 
-const store = createStore(reducers, applyMiddleware(thunk, promise));
+
 
 ReactDOM.render(
-	<Provider store={store}>
-	<App />
-	</Provider>,
+	<App />,
 	document.querySelector("#root")
 	);
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import PostsReducer from './PostsReducer';
-import NasaReducer from './nasa_reducer';
-
-export default combineReducers({
-	posts: PostsReducer,
-	astronomy : NasaReducer
-});
-
