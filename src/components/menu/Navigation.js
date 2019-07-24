62b162bb97b37d5b8a0a37e20bee8211f2c42fc4
S0f1eM <s.marchand56@gmail.com>
2019-07-24 13:59:02 +0200

    add google authentification button on header

--- src/components/menu/Navigation.js
+++ src/components/menu/Navigation.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from '../Authentification/GoogleAuth';
+
 
 const Navigation = () => {
 
@@ -12,6 +14,9 @@ const Navigation = () => {
 	             <a href="https://twitter.com/S0f1eM" target="_blank" rel="noopener noreferrer"><i className="big twitter icon"></i></a>
 	             <a href="https://www.linkedin.com/in/sophie-marchand-web-developer/" target="_blank" rel="noopener noreferrer"><i className="big linkedin icon"></i></a>
             	 <a href="https://github.com/S0f1eM" target="_blank" rel="noopener noreferrer"><i className="big github alternate icon"></i></a>
+            	</div>
+            	<div className="item">     
+            		<GoogleAuth />
             	</div>
 		  </div>
 		</div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './PostsReducer';
 import NasaReducer from './nasa_reducer';
+import authReducer from './authReducer';
+
 
 export default combineReducers({
 	posts: PostsReducer,
-	astronomy : NasaReducer
+	astronomy : NasaReducer,
+	auth: authReducer
 });
 
