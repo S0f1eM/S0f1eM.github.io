97f03e6cff786516c677284bc4407b34289e33e0
S0f1eM <s.marchand56@gmail.com>
2020-07-02 10:00:43 +0200

    failed :( quick fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,7 +18,7 @@ export const fetchPosts = () => async dispatch => {
 //fetch data from image random NASA API
 export const fetchData = () => {
 
-	const API_KEY = 'OeVrASwiJW2BEaHrrrau83wy7ekPw2L81Sc6xSpH';
+	const API_KEY = '';
 	const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key='
 	const NasaRequest = axios.get(END_POINT+API_KEY);
 
